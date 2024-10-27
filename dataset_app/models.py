ce79d67d2e65ed5d95fcce14624cda895a69b2c5
armp1379 <13ar79@gmail.com>
2024-10-27 23:14:43 +0330

    created models

--- dataset_app/models.py
+++ dataset_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Dataset(models.Model):
+    command = models.TextField()
+    statment = models.TextField()
+    created = models.DateTimeField(auto_now_add= True)
+    last_updated = models.DateTimeField(auto_now= True)
+    is_active = models.BooleanField(default= True)
+
+class Tags(models.Model):
+    dataset = models.ForeignKey(Dataset, on_delete= models.CASCADE)
+    description = models.CharField(max_length=255)
+    state = models.BooleanField()
