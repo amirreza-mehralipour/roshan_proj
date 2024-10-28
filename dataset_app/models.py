b699e00c53a6e03de8eb2fdf18456ab05263fbd7
armp1379 <13ar79@gmail.com>
2024-10-29 01:05:53 +0330

    add model TaggedDataset and migrat; and adjusted previous models slightly

--- dataset_app/models.py
+++ dataset_app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Tag(models.Model):
     description = models.CharField(max_length=255)
-    state = models.BooleanField()
+    # state = models.BooleanField()
 
     def __str__(self) -> str:
         return self.description
@@ -14,7 +15,19 @@ class Dataset(models.Model):
     created = models.DateTimeField(auto_now_add= True)
     last_updated = models.DateTimeField(auto_now= True)
     is_active = models.BooleanField(default= True)
-    tags = models.ManyToManyField(Tag)
+    # tags = models.ManyToManyField(Tag)
 
     def __str__(self) -> str:
         return f'{self.command} | {self.statment}'
+    
+
+class TaggedDatasets(models.Model):
+    dataset = models.ForeignKey(Dataset, on_delete= models.PROTECT)
+    tag = models.ForeignKey(Tag, on_delete=models.PROTECT)
+    tag_status = models.BooleanField()
+    opretor = models.ForeignKey(User,on_delete=models.PROTECT)
+    tagged_at = models.DateTimeField(auto_now_add= True)
+    updated_at = models.DateTimeField(auto_now= True)
+
+    def __str__(self) -> str:
+        return f'tag: {self.tag} dataset: {self.dataset.statment}'
