eddebc5db1068084af50452a2883acb68a643be8
armp1379 <13ar79@gmail.com>
2024-10-29 23:18:22 +0330

    registered new models in admin.py

--- dataset_app/admin.py
+++ dataset_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib.admin import register, ModelAdmin
-from .models import Dataset, Tag
+from .models import Dataset, Tag, Data, DataTag, AnsweredDataset
 
 
 @register(Dataset)
@@ -9,4 +9,18 @@ class DatasetAdmin(ModelAdmin):
 
 @register(Tag)
 class TagAdmin(ModelAdmin):
+    pass
+
+@register(DataTag)
+class DataTagAdmin(ModelAdmin):
+    pass
+
+
+@register(Data)
+class DataAdmin(ModelAdmin):
+    pass
+
+
+@register(AnsweredDataset)
+class AnsweredDatasetAdmin(ModelAdmin):
     pass
