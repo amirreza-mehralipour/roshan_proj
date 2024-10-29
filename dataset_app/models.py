de291111046e108e7163091f7ad7faa8cf0a4c0d
armp1379 <13ar79@gmail.com>
2024-10-29 23:05:41 +0330

    refactored and adje=usted and renamed some models, and flushed DB and migrated.

--- dataset_app/models.py
+++ dataset_app/models.py
@@ -1,31 +1,54 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 
 class Tag(models.Model):
     description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add= True)
+    last_updated_at = models.DateTimeField(auto_now= True)
+    
 
     def __str__(self) -> str:
         return self.description
 
-class Dataset(models.Model):
-    command = models.TextField()
+class Data(models.Model):
+    description = models.TextField()
     statement = models.TextField()
-    created = models.DateTimeField(auto_now_add= True)
-    last_updated = models.DateTimeField(auto_now= True)
+    created_at = models.DateTimeField(auto_now_add= True)
+    last_updated_at = models.DateTimeField(auto_now= True)
 
     def __str__(self) -> str:
-        return f'{self.command} | {self.statment}'
+        return f'{self.description} | {self.statement}'
     
 
-class DatasetTag(models.Model):
-    dataset = models.ForeignKey(Dataset, on_delete= models.PROTECT)
+class DataTag(models.Model):
+    data = models.ForeignKey(Data, on_delete= models.PROTECT)
     tag = models.ForeignKey(Tag, on_delete=models.PROTECT)
     tag_status = models.BooleanField()
-    operator = models.ForeignKey(User,on_delete=models.PROTECT)
-    tagged_at = models.DateTimeField(auto_now_add= True)
-    updated_at = models.DateTimeField(auto_now= True)
-    is_active = models.BooleanField(default= True)
+    created_at = models.DateTimeField(auto_now_add= True)
+    last_updated_at = models.DateTimeField(auto_now= True)
+
+
 
     def __str__(self) -> str:
-        return f'tag: {self.tag.description} dataset: {self.dataset.statment}'
+        return f'tag: {self.tag.description} dataset: {self.data.statement}'
+    
+
+class Dataset(models.Model):
+    datatag = models.ManyToManyField(DataTag)
+    is_active = models.BooleanField(default= True)
+
+    # def __str__(self) -> str:
+    #     data = self.DataTag.all()
+    #     return f'{data[0]}'
+    
+
+class AnsweredDataset(models.Model):
+    dataset = models.ForeignKey(Dataset, on_delete= models.PROTECT)
+    # operator = models.ForeignKey(User, on_delete= models.PROTECT)
+    tagged_at = models.DateTimeField(auto_now_add= True)
+    last_changes_in_tags = models.DateTimeField(auto_now= True)
+
+    # def __str__(self) -> str:
+    #     return super().__str__()
+
