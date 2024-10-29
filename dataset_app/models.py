7eef875ca608c673fdfca630b3e86559f3151bb0
armp1379 <13ar79@gmail.com>
2024-10-30 00:53:46 +0330

    adjusted models and migrated

--- dataset_app/models.py
+++ dataset_app/models.py
@@ -1,5 +1,6 @@
+from django.utils import timezone
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 
 class Tag(models.Model):
@@ -30,18 +31,24 @@ class DataTag(models.Model):
 
 
     def __str__(self) -> str:
-        return f'tag: {self.tag.description} dataset: {self.data.statement}'
+        return f'data: {self.data.statement} | tag: {self.tag.description}'
     
 
 class AnsweredDataTag(models.Model):
     datatag = models.ForeignKey(DataTag, on_delete= models.PROTECT)
-    # operator = models.ForeignKey(User, on_delete= models.PROTECT)
-    tag_status = models.BooleanField()
-    tagged_at = models.DateTimeField(auto_now_add= True)
+    operator = models.ForeignKey(User, on_delete= models.PROTECT, null= True, default= None)
+    created_at = models.DateTimeField(auto_now_add= True)
+    tag_status = models.BooleanField(null= True, default= None)
+    tagged_at = models.DateTimeField(blank= True, null= True)
     last_changed_tag_at = models.DateTimeField(auto_now= True)
+    
+    def save(self, *args, **kwargs) -> None:
+        if self.operator != None and self.tagged_at == None:
+            self.tagged_at = timezone.now()
+        super().save(*args, **kwargs)
 
-    # def __str__(self) -> str:
-    #     return super().__str__()
+    def __str__(self) -> str:
+        return f'tagged by {self.operator} on {self.datatag} with status: {self.tag_status}'
 
 
 class Dataset(models.Model):
@@ -52,4 +59,3 @@ class Dataset(models.Model):
     #     data = self.DataTag.all()
     #     return f'{data[0]}'
     
-
