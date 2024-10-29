a01519c3639af5ec1b603b1ca5508a279b59ed46
armp1379 <13ar79@gmail.com>
2024-10-29 23:22:43 +0330

    add verbose name in app.py

--- dataset_app/apps.py
+++ dataset_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DatasetAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dataset_app'
+    verbose_name = 'dataset'
